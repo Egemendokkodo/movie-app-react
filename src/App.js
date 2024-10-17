3de3e0c1c1c21a3c75a047e3f0eea887cfae86ff
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2024-10-17 18:48:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1>SA</h1><br></br><br></br>
+        <h1>AS</h1>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
